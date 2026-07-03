30a456e20cb55535577d0dc74fe6b4df23fa0410
aliceroman6666-maker <alice.roman.6666@gmail.com>
2026-07-04 00:31:00 +0300

    Create app/src/main/java/com/horsecare/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/horsecare/app/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.horsecare.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Brown80 = Color(0xFF8D6E63)
+val Brown40 = Color(0xFF5D4037)
+val Green = Color(0xFF4CAF50)
+val Amber = Color(0xFFFFA000)
+val Red = Color(0xFFD32F2F)
+val Background = Color(0xFFFAF7F2)
