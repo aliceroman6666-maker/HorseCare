6284bf7c0d65c724c7f432c4734b64f1d1ec1bef
aliceroman6666-maker <alice.roman.6666@gmail.com>
2026-07-04 00:18:24 +0300

    Create app/src/main/java/com/horsecare/app/HorseCareApp.kt

---
+++ app/src/main/java/com/horsecare/app/HorseCareApp.kt
@@ -0,0 +1,23 @@
+package com.horsecare.app
+
+import android.app.Application
+import com.horsecare.app.data.HorseCareDatabase
+import com.horsecare.app.data.repository.HorseCareRepository
+
+class HorseCareApp : Application() {
+
+    lateinit var repository: HorseCareRepository
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        val db = HorseCareDatabase.getInstance(this)
+        repository = HorseCareRepository(
+            horseDao = db.horseDao(),
+            trainingSessionDao = db.trainingSessionDao(),
+            healthRecordDao = db.healthRecordDao(),
+            contactDao = db.contactDao(),
+            reminderDao = db.reminderDao()
+        )
+    }
+}
