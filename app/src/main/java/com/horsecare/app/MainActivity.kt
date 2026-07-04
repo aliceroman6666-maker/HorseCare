89d735a400c03765347dd6bf17fbe7f458a339fe
aliceroman6666-maker <alice.roman.6666@gmail.com>
2026-07-04 22:38:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/horsecare/app/MainActivity.kt
+++ app/src/main/java/com/horsecare/app/MainActivity.kt
@@ -6,9 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.horsecare.app.ui.RepositoryViewModelFactory
 import com.horsecare.app.ui.screens.home.HomeScreen
 import com.horsecare.app.ui.screens.home.HomeViewModel
+import com.horsecare.app.ui.screens.horse.AddHorseScreen
+import com.horsecare.app.ui.screens.horse.AddHorseViewModel
 import com.horsecare.app.ui.theme.HorseCareTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,33 +21,70 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HorseCareTheme {
-                val app = application as HorseCareApp
+                val navController = rememberNavController()
 
-                // Поки що працюємо з першим конем (id = 1).
-                // Коли додамо екран вибору/додавання коня - id буде динамічним.
-                val homeViewModel: HomeViewModel = viewModel(
-                    factory = RepositoryViewModelFactory { repo ->
-                        HomeViewModel(repo, horseId = 1L)
-                    }
-                )
-                val uiState by homeViewModel.uiState.collectAsState()
+                NavHost(navController = navController, startDestination = "home") {
 
-                HomeScreen(
-                    uiState = uiState,
-                    onAddHorseClick = { /* TODO: екран додавання коня */ },
-                    onMenuClick = { /* TODO: перемикання між кіньми */ },
-                    onMarkDone = { /* TODO: відмітити виконаним */ },
-                    onReschedule = { record ->
-                        homeViewModel.rescheduleRecord(
-                            record.id,
-                            record.nextDueDate?.plusWeeks(1) ?: return@HomeScreen
+                    composable("home") {
+                        // Поки що працюємо з першим конем (id = 1).
+                        // Коли додамо перемикач - id буде динамічним.
+                        val homeViewModel: HomeViewModel = viewModel(
+                            factory = RepositoryViewModelFactory { repo ->
+                                HomeViewModel(repo, horseId = 1L)
+                            }
                         )
-                    },
-                    onOpenHealth = { /* TODO: навігація до Здоров'я */ },
-                    onOpenTraining = { /* TODO: навігація до Тренувань */ },
-                    onOpenFeeding = { /* TODO: навігація до Годування */ },
-                    onOpenProfile = { /* TODO: навігація до повного профілю */ }
-                )
+                        val uiState by homeViewModel.uiState.collectAsState()
+
+                        HomeScreen(
+                            uiState = uiState,
+                            onAddHorseClick = { navController.navigate("addHorse") },
+                            onMenuClick = { /* TODO: перемикання між кіньми */ },
+                            onMarkDone = { /* TODO: відмітити виконаним */ },
+                            onReschedule = { record ->
+                                homeViewModel.rescheduleRecord(
+                                    record.id,
+                                    record.nextDueDate?.plusWeeks(1) ?: return@HomeScreen
+                                )
+                            },
+                            onOpenHealth = { /* TODO: навігація до Здоров'я */ },
+                            onOpenTraining = { /* TODO: навігація до Тренувань */ },
+                            onOpenFeeding = { /* TODO: навігація до Годування */ },
+                            onOpenProfile = { /* TODO: навігація до повного профілю */ }
+                        )
+                    }
+
+                    composable("addHorse") {
+                        val addHorseViewModel: AddHorseViewModel = viewModel(
+                            factory = RepositoryViewModelFactory { repo ->
+                                AddHorseViewModel(repo)
+                            }
+                        )
+
+                        AddHorseScreen(
+                            onBack = { navController.popBackStack() },
+                            onSave = { name, breed, birthDate, sex, color, chipNumber,
+                                       photoUri, heightCm, weightKg, markings,
+                                       acquiredDate, sireName, damName ->
+                                addHorseViewModel.saveHorse(
+                                    name = name,
+                                    breed = breed,
+                                    birthDate = birthDate,
+                                    sex = sex,
+                                    color = color,
+                                    chipNumber = chipNumber,
+                                    photoUri = photoUri,
+                                    heightCm = heightCm,
+                                    weightKg = weightKg,
+                                    markings = markings,
+                                    acquiredDate = acquiredDate,
+                                    sireName = sireName,
+                                    damName = damName,
+                                    onSaved = { navController.popBackStack() }
+                                )
+                            }
+                        )
+                    }
+                }
             }
         }
     }
